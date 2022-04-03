0c1cc61f133ab459ca6a15027ad2dfe347859fc6
NguyenTrongPhuc1804 <102460512+NguyenTrongPhuc1804@users.noreply.github.com>
2022-04-03 12:07:34 +0700

    uadate slider

---
+++ app.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+  $('.Item-Box').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows:true,
+    autoplay:true,
+    autoplaySpeed:2000,
+    Infinity:true,
+    prevArrow:"<img class='a-left control-c prev slick-prev' src='../img/Left Arrow.png'>",
+    nextArrow:"<img class='a-right control-c next slick-next' src='../img/Right Arrow.png'>",
+    responsive: [
+      {
+        breakpoint: 1025,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+
+          arrows:true
+        }
+      },
+      {
+        breakpoint: 601,
+        settings: {
+          slidesToShow: 1,
+
+          arrows:false
+        }
+      },
+      
+    ]
+   
+  });
+}); 
--- test.js
+++
@@ -1,9 +0,0 @@
-const btnScrollTop = document.querySelector("#btnSrollTop");
-btnScrollTop.addEventListener("click", function (){
-  window.scrollTo({
-      top:0,
-      left:0,
-      behavior:"smooth"
-  });
-  
-})
